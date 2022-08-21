38c3a171b92dfee9d5cc3632a50197dfceab8796
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-21 13:43:38 +0200

    merge

--- app.js
+++ app.js
@@ -19,17 +19,18 @@ const app = express();
 require("./config")(app);
 
 // Requiring sessions.config 
-// require("./config/sessions.config")(app);
+require("./config/sessions.config")(app);
 
 // Route handling
 const index = require("./routes/index.routes");
 app.use("/", index);
 
+const authRouter = require("./routes/auth.routes");
+app.use("/auth", authRouter);
+
 const gameRouter = require("./routes/game.routes");
 app.use("/game", gameRouter);
 
-const authRoute = require("./routes/auth.routes");
-app.use("/auth", authRoute);
 
 // Error handling
 require("./error-handling")(app);
