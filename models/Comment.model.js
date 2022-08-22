bd5065a0112778e2e4b920b70a4a9c0de3f02900
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-22 12:31:02 +0200

    comments done

--- models/Comment.model.js
+++ models/Comment.model.js
@@ -4,7 +4,7 @@ const { Schema, model } = require("mongoose");
 const commentSchema = new Schema(
   {
     user: {type: Schema.Types.ObjectId, ref: "User"},
-    comment: {type: String, required: true, trim: true, maxlength: 200}
+    comment: {type: String, maxlength: 200}
   }
 );
 
