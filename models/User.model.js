a839fc67fc2bcff656a47ce8b8be90ce0d4979c7
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-23 18:27:00 +0200

    almost done

--- models/User.model.js
+++ models/User.model.js
@@ -13,7 +13,8 @@ const userSchema = new Schema(
 
     password: { type: String, required: true },
     isAdmin: {type: Boolean, default: false},
-    image: {type: String, default: ""}
+    image: {type: String, default: ""},
+    favouriteGames:[{type: Schema.Types.ObjectId, ref: "Game"}]
   }
 );
 
