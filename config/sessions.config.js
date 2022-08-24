cf6e9fd14f68ad4cdd47cc55064e3f055a61489b
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-24 17:43:59 +0200

    heroku

--- config/sessions.config.js
+++ config/sessions.config.js
@@ -1,5 +1,6 @@
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const MONGO_URI = process.env.MONGODB_URI 
 
 module.exports = app => {
 // required for the app when deployed to Heroku (in production)
@@ -14,7 +15,7 @@ app.use(
             maxAge: (1000 * 60 * 60 * 24) * 30
         },
         store: new MongoStore({
-        mongoUrl: 'process.env.MONGO_URI'
+        mongoUrl: 'MONGO_URI'
         })
     })
 )}
--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/humble-gamer";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/humble-gamer";
 
 mongoose
   .connect(process.env.MONGO_URI)
